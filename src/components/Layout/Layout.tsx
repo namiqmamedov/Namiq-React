391872d78418c5a9c2922400afb67013bb658de8
namiqmamedov <namigmm@code.edu.az>
2023-08-08 03:20:09 +0400

    Created header

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import Header from './Header/Header'
+
+const Layout = () => {
+  return (
+    <Fragment>
+        <Header/>
+    </Fragment>
+  )
+}
+
+export default Layout
