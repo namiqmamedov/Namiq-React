39264a7a1d93b3e879e3cad74f983011c3041342
namiqmamedov <namigmm@code.edu.az>
2023-08-15 00:51:45 +0400

    Add admin dashboard

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react'
 import Header from './Header/Header'
 import Routers from '../../routes/Routers'
+import { ScrollRestoration } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <Fragment>
+        <ScrollRestoration/>
         <Header/>
         <Fragment>
           <Routers/>
